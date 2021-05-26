f68c41ae32af890f0f1edead411b151c366cf80f
muhajav <javierarekpondok@gmail.com>
2021-05-26 06:31:18 -0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 </head>
 <body>
     <h1>hello world</h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam deleniti asperiores molestiae aperiam dicta nihil ut ullam veniam cum? Explicabo hic magni corrupti, ullam omnis voluptatum numquam eaque quas error?</p>
 </body>
 </html>
